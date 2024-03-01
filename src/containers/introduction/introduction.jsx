229e80bfc11cb2aea35b253fc8ea4ae4666ec780
FenrirRM <rehoudja.mael@gmail.com>
2024-03-01 21:31:04 +0100

    correction CSS

--- src/containers/introduction/introduction.jsx
+++ src/containers/introduction/introduction.jsx
@@ -1,12 +1,12 @@
-import { useTheme } from '../../utils/hooks/index'
-import DownloadBtn from '../../components/downloadbtn/downloadbtn'
-import GitHub from '../../assets/icones/github.png'
-import DarkGitHub from '../../assets/icones/darkgithub.png'
+import { useTheme } from "../../utils/hooks/index";
+import DownloadBtn from "../../components/downloadbtn/downloadbtn";
+import GitHub from "../../assets/icones/github.png";
+import DarkGitHub from "../../assets/icones/darkgithub.png";
 
-import '../../sass/containers/_introduction.scss'
+import "../../sass/containers/_introduction.scss";
 
 const Introduction = () => {
-  const { darkMode } = useTheme()
+  const { darkMode } = useTheme();
 
   return (
     <section className="intro">
@@ -15,19 +15,21 @@ const Introduction = () => {
       </h2>
       <div className="intro__desc">
         <p>
-          Je suis Mael Rehoudja, développeur web junior basé en côte d'or. Afin de suivre 
-          ma passion pour les nouvelles technologies et la programmation, j’ai
-          intégré la formation certifiante intégrateur web niveau Bac +2
-          d’OpenClassrooms au cours de laquelle j’ai acquis des compétences
-          solides en HTML, CSS, JavaScript et React.  
+          Je suis Mael Rehoudja, développeur web junior basé en côte d'or. Afin
+          de suivre ma passion pour les nouvelles technologies et la
+          programmation, j’ai intégré la formation certifiante intégrateur web
+          niveau Bac +2 d’OpenClassrooms au cours de laquelle j’ai acquis des
+          compétences solides en HTML, CSS, JavaScript et React.
         </p>
         <p>
-        En tant que développeur Front-end,  je suis constamment en train d'apprendre et 
-        de m'adapter aux dernières technologies pour offrir des solutions de qualité à mes clients.
+          En tant que développeur Front-end, je suis constamment en train
+          d'apprendre et de m'adapter aux dernières technologies pour offrir des
+          solutions de qualité à mes clients.
         </p>
         <p>
           Je suis très enthousiaste à l’idée de découvrir de nouvelles
-          opportunités et collaborations pour créer des solutions web de qualité.
+          opportunités et collaborations pour créer des solutions web de
+          qualité.
           <br />
           Contactez-moi pour discuter de votre projet.
         </p>
@@ -39,12 +41,11 @@ const Introduction = () => {
             />
           </a>
           
-          
         </div>
         <DownloadBtn />
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Introduction
+export default Introduction;
